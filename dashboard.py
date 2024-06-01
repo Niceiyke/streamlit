0fff91878dc4892704d786d0af30c90d53a1f422
Niceiyke <74918186+Niceiyke@users.noreply.github.com>
2024-06-01 16:28:44 +0100

    update

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,6 @@
+import sqlite3
 import pandas as pd
-import streamlit as st 
+import streamlit as st
 import plotly.express as px
 from datetime import datetime, timedelta
 
@@ -8,7 +9,11 @@ st.set_page_config(page_title="PM Dashboard", page_icon=':bar_chart:', layout='w
 @st.cache_data
 def get_data_from_excel():
     try:
-        df = pd.read_excel(io="downtime.xlsx", sheet_name="Line14archive", engine="openpyxl")
+        conn = sqlite3.connect('line_database.db')
+        cursor = conn.cursor()
+        query = 'SELECT * FROM production_data WHERE Category IN ("Breakdown", "Minor stops")'
+        df = pd.read_sql_query(query, conn)
+        conn.close()
     except FileNotFoundError:
         st.error("The file 'downtime.xlsx' was not found.")
         st.stop()
@@ -17,17 +22,14 @@ def get_data_from_excel():
         st.stop()
     return df
 
-df1 = get_data_from_excel()
+df = get_data_from_excel()
 
-if df1.empty:
+if df.empty:
     st.warning("The dataframe is empty. Please check the Excel file and the sheet name.")
     st.stop()
 
-# Filter data
-df = df1[(df1['Category'] == "Breakdown Time") | (df1['Category'] == "Minor stops & Speed losses")]
-
 with open('style.css') as f:
-    st.markdown(f'<style> {f.read()} </style>',unsafe_allow_html=True)
+    st.markdown(f'<style> {f.read()} </style>', unsafe_allow_html=True)
 
 st.title(":bar_chart: PM TEAM AMA DASHBOARD")
 
@@ -35,7 +37,7 @@ st.title(":bar_chart: PM TEAM AMA DASHBOARD")
 df['ProductionDate'] = pd.to_datetime(df['ProductionDate'], errors='coerce')
 
 # Ensure these columns exist before filtering
-required_columns = ['ProductionDate', 'Line', 'Category', 'SubCategory', 'Frequency', 'Duration', 'FailureDesc']
+required_columns = ['ProductionDate', 'Line', 'Category', 'Equipment', 'Frequency', 'Duration', 'FunctionFailure']
 for col in required_columns:
     if col not in df.columns:
         st.error(f"Column '{col}' is missing in the dataframe.")
@@ -45,6 +47,12 @@ df = df[required_columns]
 
 # Sidebar filters
 st.sidebar.header("Data Filters")
+
+# Calculate yesterday's date and today's date
+yesterday = datetime.now() - timedelta(1)
+today = datetime.now()
+
+# Initialize filters with the complete dataset
 line = st.sidebar.multiselect('Select Line', options=df['Line'].unique(), default=[1])
 
 # Calculate yesterday's date and today's date
@@ -52,7 +60,13 @@ yesterday = datetime.now() - timedelta(1)
 today = datetime.now()
 
 date_range = st.sidebar.date_input("Select Date Range", value=[yesterday.date(), today.date()])
-equipment = st.sidebar.multiselect('Select Equipment', options=df['SubCategory'].unique(), default=df['SubCategory'].unique())
+
+
+# Update available options for 'Equipment' based on selected lines
+filtered_df = df[df['Line'].isin(line)]
+equipment_options = filtered_df['Equipment'].unique()
+equipment = st.sidebar.multiselect('Select Equipment', options=equipment_options, default=equipment_options)
+
 
 # Check if all filters are selected
 if not line:
@@ -73,9 +87,9 @@ end_date = datetime.combine(date_range[1], datetime.max.time())
 
 # Query the data based on selections
 df = df[(df['Line'].isin(line)) & 
-         (df['ProductionDate'] >= start_date) & 
-         (df['ProductionDate'] <= end_date) &
-         (df['SubCategory'].isin(equipment))]
+        (df['ProductionDate'] >= start_date) & 
+        (df['ProductionDate'] <= end_date) &
+        (df['Equipment'].isin(equipment))]
 
 # Check if filtered dataframe is empty
 if df.empty:
@@ -83,8 +97,8 @@ if df.empty:
     st.stop()
 
 # Separate data based on Category
-df_bd = df[df['Category'] == "Breakdown Time"]
-df_ms = df[df['Category'] == "Minor stops & Speed losses"]
+df_bd = df[df['Category'] == "Breakdown"]
+df_ms = df[df['Category'] == "Minor stops"]
 
 # Metrics calculation
 total_num_breakdown = df_bd['Line'].count()
@@ -100,29 +114,26 @@ num_breakdown_60mins = df_bd[df_bd['Duration'] >= 60]['Line'].count()
 first_column, second_column, third_column = st.columns(3)
 
 with first_column:
-
-    st.metric(label="Total Number Of Breakdowns",value=total_num_breakdown)
-    st.metric(label="Total Duration Of Breakdown",value=total_duration_breakdown)
+    st.metric(label="Total Number Of Breakdowns", value=total_num_breakdown)
+    st.metric(label="Total Duration Of Breakdown", value=total_duration_breakdown)
 
 with second_column:
-    st.metric(label="Total Number Of Minor Stops",value=total_num_minor_stops)
-    st.metric(label="Total Duration Of Minor Stops",value=total_duration_minor_stops)
-
+    st.metric(label="Total Number Of Minor Stops", value=total_num_minor_stops)
+    st.metric(label="Total Duration Of Minor Stops", value=total_duration_minor_stops)
 
 with third_column:
-    st.metric(label="No. Of Breakdowns >= 60 mins",value=num_breakdown_60mins)
-    st.metric(label="Total Duration Of Breakdowns >= 60 mins",value=num_breakdown_60mins)
-
+    st.metric(label="No. Of Breakdowns >= 60 mins", value=num_breakdown_60mins)
+    st.metric(label="Total Duration Of Breakdowns >= 60 mins", value=num_breakdown_60mins)
 
 # Bar chart
-bd_by_equipment = df_bd.groupby(by=['SubCategory'])[['Frequency', 'Duration']].sum().sort_values(by=['Duration', 'Frequency'], ascending=False)
-ms_by_equipment = df_ms.groupby(by=['SubCategory'])[['Frequency', 'Duration']].sum().sort_values(by=['Duration', 'Frequency'], ascending=False)
+bd_by_equipment = df_bd.groupby(by=['Equipment'])[['Frequency', 'Duration']].sum().sort_values(by=['Duration', 'Frequency'], ascending=False)
+ms_by_equipment = df_ms.groupby(by=['Equipment'])[['Frequency', 'Duration']].sum().sort_values(by=['Duration', 'Frequency'], ascending=False)
 
-# Group by 'SubCategory' and calculate the sum of 'Frequency' and 'Duration'
-grouped_data_bd = df_bd.groupby(by=['SubCategory'])[['Frequency', 'Duration']].sum().reset_index().sort_values(by=['Duration', 'Frequency'], ascending=False)
-grouped_data_bdf = df_bd.groupby(by=['SubCategory', 'FailureDesc'])[['Frequency', 'Duration']].sum().reset_index().sort_values(by=['Duration', 'Frequency'], ascending=False)
-grouped_data_ms = df_ms.groupby(by=['SubCategory'])[['Frequency', 'Duration']].sum().reset_index().sort_values(by=['Duration', 'Frequency'], ascending=False)
-grouped_data_msf = df_ms.groupby(by=['SubCategory', 'FailureDesc'])[['Frequency', 'Duration']].sum().reset_index().sort_values(by=['Duration', 'Frequency'], ascending=False)
+# Group by 'Equipment' and calculate the sum of 'Frequency' and 'Duration'
+grouped_data_bd = df_bd.groupby(by=['Equipment'])[['Frequency', 'Duration']].sum().reset_index().sort_values(by=['Duration', 'Frequency'], ascending=False)
+grouped_data_bdf = df_bd.groupby(by=['Equipment', 'FunctionFailure'])[['Frequency', 'Duration']].sum().reset_index().sort_values(by=['Duration', 'Frequency'], ascending=False)
+grouped_data_ms = df_ms.groupby(by=['Equipment'])[['Frequency', 'Duration']].sum().reset_index().sort_values(by=['Duration', 'Frequency'], ascending=False)
+grouped_data_msf = df_ms.groupby(by=['Equipment', 'FunctionFailure'])[['Frequency', 'Duration']].sum().reset_index().sort_values(by=['Duration', 'Frequency'], ascending=False)
 
 bd_column, ms_column = st.columns(2)
 
@@ -132,37 +143,37 @@ fig_ms = px.bar(ms_by_equipment, x=ms_by_equipment.index, y="Duration", title="M
 # Create a grouped bar chart using Plotly Express
 fig_grouped_bar_bd = px.bar(
     grouped_data_bd,
-    x='SubCategory',
+    x='Equipment',
     y=['Frequency', 'Duration'],
     barmode='group',
-    title="Breakdown Failure Modes",
+    title="Breakdown Deployment",
     text_auto=True
 )
 
 fig_grouped_bar_ms = px.bar(
     grouped_data_ms,
-    x='SubCategory',
+    x='Equipment',
     y=['Frequency', 'Duration'],
     barmode='group',
-    title="Minor Stop Failure Modes",
+    title="Minor Stop Deployment",
     text_auto=True
 )
 
 fig_grouped_bar_bd_failures = px.bar(
     grouped_data_bdf,
-    x='FailureDesc',
+    x='FunctionFailure',
     y=['Frequency', 'Duration'],
     barmode='group',
-    title="Breakdown Failure Modes",
+    title="Breakdown Function Failures",
     text_auto=True
 )
 
 fig_grouped_bar_ms_failures = px.bar(
     grouped_data_msf,
-    x='FailureDesc',
+    x='FunctionFailure',
     y=['Frequency', 'Duration'],
     barmode='group',
-    title="Minor Stop Failure Modes",
+    title="Minor Stop Function Failures",
     text_auto=True
 )
 
---
+++ linesql.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import sqlite3
+
+# Step 1: Read the Excel file
+excel_file_path = 'archive.xlsx'
+df = pd.read_excel(io=excel_file_path,sheet_name='Sheet1',engine='openpyxl')
+print("done readinf excel file")
+
+# Step 2: Create a SQLite database connection
+conn = sqlite3.connect('line_database.db')
+cursor = conn.cursor()
+
+# Step 3: Create a table in the SQLite database
+create_table_query = '''
+CREATE TABLE IF NOT EXISTS production_data (
+    ProductionDate TEXT,
+    FunctionFailure TEXT,
+    Equipment TEXT,
+    Line Integer,
+    Frequency INTEGER,
+    Duration INTEGER,
+    Category TEXT
+)
+'''
+cursor.execute(create_table_query)
+conn.commit()
+
+# Step 4: Insert the data into the table
+df.to_sql('production_data', conn, if_exists='append', index=False)
+
+# Step 5: Verify the insertion (optional)
+cursor.execute('SELECT * FROM production_data')
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+# Close the database connection
+conn.close()
